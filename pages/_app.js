9fbf8fc08e5826b56e4fed382d321c61a3edc190
Sungchul Park <parksungchul@Sungchuls-Mac-mini.local>
2021-09-17 03:12:58 -0400

    up to styled JSX

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import Layout from '../components/layout'
 import '../styles/globals.css'
+//global.css should be only imported to app.js
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} /> {/* every page */}
+    </Layout>
+  )
 }
 
 export default MyApp
