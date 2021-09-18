43eec1861ad303edd2649a5e1e73434727a92360
Sungchul Park <parksungchul@Sungchuls-Mac-mini.local>
2021-09-17 20:49:21 -0400

    Data fetching

---
+++ pages/article/[id]/index.js
@@ -0,0 +1,67 @@
+//this is going to be a single id page
+import Link from 'next/link'
+import { useRouter } from "next/router" 
+
+//to make id
+
+const article = ({article}) => {
+    //opiton 1
+    // const router = useRouter()  //set router
+    // const {id} = router.query //destructure it
+    //^This can be used to get an {id}
+    return(
+        <>
+            <h1>{article.title}</h1>
+            <p>{article.body}</p>
+            <br />
+            <Link href='/'>Go Back</Link>
+        </>
+    ) 
+}
+
+//option 2
+/*
+export const getServerSideProps = async (context) => {
+    //context parameter is an object containing keys such as res, params, req, etc.
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+
+    const article = await res.json()
+
+    return {
+        props: {
+            article
+        }
+    }
+}
+*/
+
+//option 3
+export const getStaticProps = async (context) => {
+    //context parameter is an object containing keys such as res, params, req, etc.
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+
+    const article = await res.json()
+
+    return {
+        props: {
+            article
+        }
+    }
+}
+
+export const getStaticPaths = async () => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const articles = await res.json()
+
+    const ids = articles.map(article => article.id)
+    //store each id to an array
+    const paths = ids.map(id => ({params: {id: id.toString()}}))
+    
+    //return an Object with params, return an id key, the value is stringified id.
+    return {
+        paths,
+        fallback: false //if the user goes to params that doesn't exist it will print 404 page
+    }
+}
+
+export default article
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head'
 //this is used for custom titles or meta tags, keywords, etc
+import ArticleList from '../components/ArticleList'
 
-export default function Home() {
+export default function Home({ articles }) {
+  // console.log(articles)
   return (
     <div>
       <Head>
@@ -9,10 +11,36 @@ export default function Home() {
         <meta name='keywords' content='web development, programming' />
       </Head>
 
-
-      <h1>Welcome to Next</h1>
+      <ArticleList articles={articles}/>
     </div>
   )
 }
 
-export const 
+/*Props way
+export default function Home(props) {
+  // console.log(articles)
+  return (
+    <div>
+      <Head>
+        <title>WebDeb Newz</title>
+        <meta name='keywords' content='web development, programming' />
+      </Head>
+
+      <ArticleList articles={props.articles}/>
+    </div>
+  )
+}
+*/
+
+//Data fetching functions can be written above or below a component.
+//async is for await
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles
+    }
+  }
+}
